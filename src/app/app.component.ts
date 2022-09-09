014979632943893c24dd246892c9fad6541867e1
Kuba Dospial <jakub.dospial@gmail.com>
2022-09-09 19:38:58 +0200

    added graphql

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'graphql';
+  ships$ = this.apollo
+    .watchQuery({
+      query: gql`
+        query Ships {
+          ships {
+            model
+            name
+            type
+            status
+          }
+        }
+      `,
+    })
+    .valueChanges.pipe(
+      filter(Boolean),
+      map((response: any) => response.data['ships'])
+    );
+
+  constructor(private apollo: Apollo) {}
 }
